8b802a793ab47482311baa066eb85d58f66604cc
GabrielRezes <gabriel.rezes@gmail.com>
2022-03-21 22:39:32 -0300

    add section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Button from '../components/Button';
 import '../styles/Container.scss'
 
 const products = [
-  {id: 1, name: 'iphone 8', img:'./'},
-  {id: 2, name: 'iphone 9', img:'./'},
-  {id: 3, name: 'iphone 10', img:'./'},
-  {id: 4, name: 'iphone 11', img:'./'},
-  {id: 5, name: 'iphone 12', img:'./'},
-  {id: 6, name: 'iphone 12PRO', img:'./'},
+  {id: 1, name: 'iphone 8', price: 'R$ 2.000', img:'https://www.pngmart.com/files/15/Apple-iPhone-11-PNG-Pic.png'},
+  {id: 2, name: 'iphone 9', price: 'R$ 2.000', img:'https://www.pngmart.com/files/15/Apple-iPhone-11-PNG-Pic.png'},
+  {id: 3, name: 'iphone 10', price: 'R$ 2.000', img:'https://www.pngmart.com/files/15/Apple-iPhone-11-PNG-Pic.png'},
+  {id: 4, name: 'iphone 11', price: 'R$ 2.000', img:'https://www.pngmart.com/files/15/Apple-iPhone-11-PNG-Pic.png'},
+  {id: 5, name: 'iphone 12', price: 'R$ 2.000', img:'https://www.pngmart.com/files/15/Apple-iPhone-11-PNG-Pic.png'},
+  {id: 6, name: 'iphone 12PRO', price: 'R$ 2.000', img:'https://www.pngmart.com/files/15/Apple-iPhone-11-PNG-Pic.png'},
 ]
 
 export default function Home () {
+
+  const [ currProductImage, setCurrProductImage ] = useState<any>(0);
+
+  const addProduct = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+  }
+
   return (
     <div className='container'>
-      <h1>PRODUTOS</h1>
-      <div className='container-products'>
-        {products.map(product => {
-          return (
-          <ul className='card-products'>
-            <li>product.name</li>
-            <li>product.img</li>
-          </ul>
-          )
-        }) }
-      </div>
+      <h1 className='title'>PRODUTOS</h1>
+        <ul className='container-products'>
+          <li className='product'>
+            <span className='title-product'>{products[currProductImage].name}</span>
+            <span className='title-product'>{products[currProductImage].price}</span>
+            <img className='img-product' src={products[currProductImage].img}/>
+          </li>
+          <Button onClick={addProduct} text="Adicionar ao carrinho" variant='secondary' />
+        </ul> 
     </div>
   );
 };
