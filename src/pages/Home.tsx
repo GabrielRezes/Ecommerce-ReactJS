ec074d59f0959d8d122757262aeed984090d2b84
GabrielRezes <gabriel.rezes@gmail.com>
2022-03-22 08:49:57 -0300

    add images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,56 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
+import useSizeWindow from '../hooks/useSizeWindow';
 import Button from '../components/Button';
+
+import { IoChevronBack, IoChevronForward } from 'react-icons/io5';
 import '../styles/Container.scss'
 
 const products = [
-  {id: 1, name: 'iphone 8', price: 'R$ 2.000', img:'https://www.pngmart.com/files/15/Apple-iPhone-11-PNG-Pic.png'},
-  {id: 2, name: 'iphone 9', price: 'R$ 2.000', img:'https://www.pngmart.com/files/15/Apple-iPhone-11-PNG-Pic.png'},
-  {id: 3, name: 'iphone 10', price: 'R$ 2.000', img:'https://www.pngmart.com/files/15/Apple-iPhone-11-PNG-Pic.png'},
-  {id: 4, name: 'iphone 11', price: 'R$ 2.000', img:'https://www.pngmart.com/files/15/Apple-iPhone-11-PNG-Pic.png'},
-  {id: 5, name: 'iphone 12', price: 'R$ 2.000', img:'https://www.pngmart.com/files/15/Apple-iPhone-11-PNG-Pic.png'},
-  {id: 6, name: 'iphone 12PRO', price: 'R$ 2.000', img:'https://www.pngmart.com/files/15/Apple-iPhone-11-PNG-Pic.png'},
+  {id: 1, name: 'iphone 8', price: 'R$ 2.000', img:'https://m.media-amazon.com/images/I/516LM9NTSfL._AC_SY879_.jpg'},
+  {id: 2, name: 'iphone 9', price: 'R$ 3.999', img:'https://m.media-amazon.com/images/I/610D42wRbWL._AC_SX679_.jpg'},
+  {id: 3, name: 'iphone 10', price: 'R$ 4.999', img:'https://m.media-amazon.com/images/I/41WyKQBoPoL._AC_.jpg'},
+  {id: 4, name: 'iphone 11', price: 'R$ 5.999', img:'https://m.media-amazon.com/images/I/61vIgEPtSEL._AC_SX679_.jpg'},
 ]
 
 export default function Home () {
 
   const [ currProductImage, setCurrProductImage ] = useState<any>(0);
+  // const [ size, sizeWindow] = useSizeWindow()
+
+  // console.log(size)
+
+  // useEffect(() => {
+  //   let size = SizeWindow()
+  //   console.log(size)    
+  // },[])
+
 
   const addProduct = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
+  };
 
-  }
+  const previousProduct = () => {
+    if(currProductImage === 0 ) return;
+    setCurrProductImage(currProductImage - 1);
+  };
+
+  const nextProduct = () => {
+    if(currProductImage === products.length - 1) return;
+    setCurrProductImage(currProductImage + 1);
+  };
+
+  // console.log(currProductImage);
+  // console.log(products.length);
 
   return (
     <div className='container'>
       <h1 className='title'>PRODUTOS</h1>
         <ul className='container-products'>
+          <div className='btn-products'>
+            <span onClick={previousProduct} className='btn left'><IoChevronBack/></span>
+            <span onClick={nextProduct} className='btn right'><IoChevronForward/></span>
+          </div>
           <li className='product'>
             <span className='title-product'>{products[currProductImage].name}</span>
             <span className='title-product'>{products[currProductImage].price}</span>
