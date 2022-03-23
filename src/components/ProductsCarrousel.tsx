20d90a78bdb03804043594b686adc84321f70f5a
GabrielRezes <gabriel.rezes@gmail.com>
2022-03-23 18:22:46 -0300

    create component

---
+++ src/components/ProductsCarrousel.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Button from './Button';
+
+import { IoChevronBack, IoChevronForward } from 'react-icons/io5';
+import '../styles/Products.scss';
+
+const products = [
+  {id: 1, name: 'iphone 8', price: 'R$ 2.000', img:'https://m.media-amazon.com/images/I/516LM9NTSfL._AC_SY879_.jpg'},
+  {id: 2, name: 'iphone 9', price: 'R$ 3.999', img:'https://m.media-amazon.com/images/I/610D42wRbWL._AC_SX679_.jpg'},
+  {id: 3, name: 'iphone 10', price: 'R$ 4.999', img:'https://m.media-amazon.com/images/I/41WyKQBoPoL._AC_.jpg'},
+  {id: 4, name: 'iphone 11', price: 'R$ 5.999', img:'https://m.media-amazon.com/images/I/61vIgEPtSEL._AC_SX679_.jpg'},
+];
+
+interface addProductProps {
+  addProduct: () => void
+}
+
+export default function ProductsCarrousel ({ addProduct }: addProductProps) {
+  const [ currProductImage, setCurrProductImage ] = useState<number>(0);
+
+  const handleClick = (event) => {
+    if(event.target.id === 'previous' && currProductImage > 0) setCurrProductImage(currProductImage - 1)
+    if(event.target.id === 'next' && currProductImage < products.length - 1) setCurrProductImage(currProductImage + 1)
+  }
+
+  return(
+    <>
+      <div onClick={handleClick} className='btn-products'>
+        <IoChevronBack className='btn-change-product left' id='previous'/>
+        <IoChevronForward className='btn-change-product right' id='next'/>
+      </div>
+  
+      <li className='card-product'>
+          <span className='title-product'>{products[currProductImage].name}</span>
+          <span className='title-product'>{products[currProductImage].price}</span>
+          <img className='img-product' src={products[currProductImage].img}/>
+          <Button onClick={addProduct} text="Adicionar ao carrinho" variant='secondary'/>
+      </li>
+    </>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,62 +1,26 @@
 import React, { useEffect, useState } from 'react';
 
-// import useSizeWindow from '../hooks/useSizeWindow';
+import ProductsCarrousel from '../components/ProductsCarrousel';
+import Products from '../components/Products'
+import useSizeWindow from '../hooks/useSizeWindow';
 import Button from '../components/Button';
 
-import { IoChevronBack, IoChevronForward } from 'react-icons/io5';
-import '../styles/Container.scss'
-
-const products = [
-  {id: 1, name: 'iphone 8', price: 'R$ 2.000', img:'https://m.media-amazon.com/images/I/516LM9NTSfL._AC_SY879_.jpg'},
-  {id: 2, name: 'iphone 9', price: 'R$ 3.999', img:'https://m.media-amazon.com/images/I/610D42wRbWL._AC_SX679_.jpg'},
-  {id: 3, name: 'iphone 10', price: 'R$ 4.999', img:'https://m.media-amazon.com/images/I/41WyKQBoPoL._AC_.jpg'},
-  {id: 4, name: 'iphone 11', price: 'R$ 5.999', img:'https://m.media-amazon.com/images/I/61vIgEPtSEL._AC_SX679_.jpg'},
-]
+import '../styles/Products.scss'
 
 export default function Home () {
-
-  const [ currProductImage, setCurrProductImage ] = useState<any>(0);
-  // const [ size, sizeWindow] = useSizeWindow()
-
-  // console.log(size)
-
-  // useEffect(() => {
-  //   let size = SizeWindow()
-  //   console.log(size)    
-  // },[])
-
-
-  const addProduct = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
+  const [ sizeWindow ] = useSizeWindow()
+  
+  const addProduct = () => {
+    console.log('oi')
   };
-
-  const previousProduct = () => {
-    if(currProductImage === 0 ) return;
-    setCurrProductImage(currProductImage - 1);
-  };
-
-  const nextProduct = () => {
-    if(currProductImage === products.length - 1) return;
-    setCurrProductImage(currProductImage + 1);
-  };
-
-  // console.log(currProductImage);
-  // console.log(products.length);
 
   return (
-    <div className='container'>
-      <h1 className='title'>PRODUTOS</h1>
+    <div className='container-page'>
+      <h1 className='title'>Produtos</h1>
         <ul className='container-products'>
-          <div className='btn-products'>
-            <span onClick={previousProduct} className='btn left'><IoChevronBack/></span>
-            <span onClick={nextProduct} className='btn right'><IoChevronForward/></span>
-          </div>
-          <li className='product'>
-            <span className='title-product'>{products[currProductImage].name}</span>
-            <span className='title-product'>{products[currProductImage].price}</span>
-            <img className='img-product' src={products[currProductImage].img}/>
-          </li>
-          <Button onClick={addProduct} text="Adicionar ao carrinho" variant='secondary' />
+          {sizeWindow.width && sizeWindow.width < 999 
+          ? <ProductsCarrousel addProduct={addProduct}/>
+          : <Products addProduct={addProduct}/>}
         </ul> 
     </div>
   );
