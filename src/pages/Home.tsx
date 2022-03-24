d2f98d21b045c54f4aa0b8bd88f5e3c5e8a83831
GabrielRezes <gabriel.rezes@gmail.com>
2022-03-24 08:26:08 -0300

    add products component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,8 @@
-import React, { useEffect, useState } from 'react';
-
-import ProductsCarrousel from '../components/ProductsCarrousel';
-import Products from '../components/Products'
-import useSizeWindow from '../hooks/useSizeWindow';
-import Button from '../components/Button';
-
+import Products from '../components/Products/Products';
 import '../styles/Products.scss'
 
 export default function Home () {
-  const [ sizeWindow ] = useSizeWindow()
-  
-  const addProduct = () => {
-    console.log('oi')
-  };
-
   return (
-    <div className='container-page'>
-      <h1 className='title'>Produtos</h1>
-        <ul className='container-products'>
-          {sizeWindow.width && sizeWindow.width < 999 
-          ? <ProductsCarrousel addProduct={addProduct}/>
-          : <Products addProduct={addProduct}/>}
-        </ul> 
-    </div>
+    <Products/>
   );
 };
