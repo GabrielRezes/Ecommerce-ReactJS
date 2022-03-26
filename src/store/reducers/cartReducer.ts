cd16f85ac1ebec5a673aa5e2c61b7e8ddf11a6be
GabrielRezes <gabriel.rezes@gmail.com>
2022-03-26 01:03:08 -0300

    added redux

---
+++ src/store/reducers/cartReducer.ts
@@ -0,0 +1,17 @@
+// export default(state = {}, { type, payload}) => {
+//   switch(type){
+//     case 'ADD_PRODUCT':
+//       return {
+
+//       }
+
+//     case 'REMOVE_PRODUCT':
+//       return {
+
+//       }  
+
+//     default {
+//       return state
+//     }  
+//   }
+// }
