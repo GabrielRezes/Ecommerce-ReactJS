4c362a1c7f94839a3709fc1993d080a092a08e3e
GabrielRezes <gabriel.rezes@gmail.com>
2022-03-29 18:02:40 -0300

    added route to cart

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,15 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Home from '../pages/Home';
-import Cart from '../pages/Cart';
-import Login from '../pages/Login';
+import Home from '../pages/Home/Home';
+import Cart from '../pages/Cart/Cart';
+import About from '../pages/About/About';
+import Login from '../pages/Login/Login';
 
 export default function AppRoutes () {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/about' element={<About/>}/>
       <Route path='/login' element={<Login/>}/>
+      <Route path='/cart' element={<Cart/>}/>
     </Routes>
   );
 };
