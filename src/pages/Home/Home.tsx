98e57d8c0d9c49a1eca35ad21ea97bf4c01ac17b
GabrielRezes <gabriel.rezes@gmail.com>
2022-03-30 17:54:28 -0300

    added carousel component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,36 +1,23 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { addProduct, removeProduct } from '../../redux/actions/products';
 
 import ProductsCarousel from '../../components/ProductsCarousel/ProductsCarousel';
-import CardProducts from '../../components/CardProducts/CardProducts';
 import TitlePage from '../../components/TitlePage/TitlePage';
-import Button from '../../components/Button/Button';
+import Products from '../../components/Products/Products';
 
-
+import { PropsProduct } from '../../types';
+import { productList } from '../../mocks/products';
 import useSizeWindow from '../../hooks/useSizeWindow';
 
 import './home.scss';
 
-const products = [
-  {id: 1, name: 'iphone 8', price: 'R$ 2.000', img:'https://m.media-amazon.com/images/I/516LM9NTSfL._AC_SY879_.jpg'},
-  {id: 2, name: 'iphone 9', price: 'R$ 3.999', img:'https://m.media-amazon.com/images/I/610D42wRbWL._AC_SX679_.jpg'},
-  {id: 3, name: 'iphone 10', price: 'R$ 4.999', img:'https://m.media-amazon.com/images/I/516LM9NTSfL._AC_SY879_.jpg'},
-  {id: 4, name: 'iphone 11', price: 'R$ 5.999', img:'https://m.media-amazon.com/images/I/610D42wRbWL._AC_SX679_.jpg'},
-];
-
-type PropsProduct = {
-  id: number,
-  name: string,
-  price: string,
-  img: string
-}
-
-export default function Products () {
+export default function Home () {
   const dispatch = useDispatch();
   const { isMobile } = useSizeWindow()
 
-  const addProductToCart = (product : PropsProduct) => {
+  const addProductToCart = (product : PropsProduct) =>  {
+    console.log(product)
     dispatch(addProduct(product));
   };
 
@@ -38,8 +25,8 @@ export default function Products () {
     <section className="container-products">
       <TitlePage title="Produtos"/>
       { isMobile 
-          ? <ProductsCarousel addProduct={addProductToCart}/>
-          : <CardProducts products={products} addProduct={addProductToCart}/>
+          ? <ProductsCarousel products={productList} add={addProductToCart}/>
+          : <Products products={productList} add={addProductToCart}/>
       }
     </section>
   );
