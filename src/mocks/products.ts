6641ce76a260933d6ede453d2e1cbe7fcef4d784
GabrielRezes <gabriel.rezes@gmail.com>
2022-04-01 11:11:59 -0300

    creating client folder

--- src/mocks/products.ts
+++
@@ -1,8 +0,0 @@
-const productList = [
-  {id: 1, name: 'iphone 8', price: 'R$ 2.000', img:'https://m.media-amazon.com/images/I/516LM9NTSfL._AC_SY879_.jpg'},
-  {id: 2, name: 'iphone 9', price: 'R$ 3.999', img:'https://m.media-amazon.com/images/I/610D42wRbWL._AC_SX679_.jpg'},
-  {id: 3, name: 'iphone 10', price: 'R$ 4.999', img:'https://m.media-amazon.com/images/I/516LM9NTSfL._AC_SY879_.jpg'},
-  {id: 4, name: 'iphone 11', price: 'R$ 5.999', img:'https://m.media-amazon.com/images/I/610D42wRbWL._AC_SX679_.jpg'},
-];
-
-export { productList };
--- src/types/index.ts
+++
@@ -1,6 +0,0 @@
-export type PropsProduct = {
-  id: number,
-  name: string,
-  price: string,
-  img: string
-};
