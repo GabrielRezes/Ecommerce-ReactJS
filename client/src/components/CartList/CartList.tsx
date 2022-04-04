8537d0094c812c1acfdeb62b4718b50c2741b4c3
GabrielRezes <gabriel.rezes@gmail.com>
2022-04-04 00:42:18 -0300

    adding state with total value

--- client/src/components/CartList/CartList.tsx
+++ client/src/components/CartList/CartList.tsx
@@ -1,14 +1,31 @@
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { PropsProduct } from '../../types';
 
 import './cartList.scss';
 
+// type Cart = {
+//   cart: PropsProduct[],
+// }
+
 export default function CartList () {
   const { cart }:any = useSelector(store => store);
+  const [ amount, setAmount ] = useState<number>(0);
+
+  useEffect(() => {
+    let total = cart.reduce((acc:any, curr:any) => {
+      let totalSumOfProduct = (Number(curr.price.replace(/[^0-9,]/g, ''))); 
+      return acc + totalSumOfProduct;
+    }, 0);
+
+    setAmount(total); 
+  },[cart])
 
   return (
     <div className="container-cart">
       <ul className="list">
-        {cart.map((product:any, index:any) => {
+      <h2> Total: {amount}</h2> 
+        {cart.map((product:PropsProduct, index:number) => {
           return (
             <li className="card" key={index}>
 
@@ -17,12 +34,16 @@ export default function CartList () {
                 <p>{product.name}</p>
               </div>
 
-              <div className="info">
-                <p>{product.price}</p>  
-                <span className="total">{}</span>
+              <div className="info-product">
+                <p className="name-product">{product.price}</p>
+                <div className="qnt-product">
+                  <button className="btn"> - </button>
+                    <span>{product.qnt}</span>
+                  <button className="btn"> + </button>
+                </div>  
               </div>
 
-            </li>
+            </li> 
           )
         })}
       </ul>
