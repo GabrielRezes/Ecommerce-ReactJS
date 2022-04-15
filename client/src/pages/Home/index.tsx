1e75c0fd7764f90b99d6e63863702736591a71ef
GabrielRezes <gabriel.rezes@gmail.com>
2022-04-14 23:19:43 -0300

    adding dispatch to set loading and try/catch

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { addProductAction, removeProductAction } from '../../redux/actions/products';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addProductToCart, removeProductToCart } from '../../redux/actions/cartActions';
+import { setProducts, setLoading } from '../../redux/actions/productActions';
 
 import ProductsCarousel from '../../components/ProductsCarousel';
 import TitlePage from '../../components/TitlePage';
@@ -10,34 +11,65 @@ import { PropsProduct } from '../../types';
 import { productList } from '../../mocks/products';
 import useSizeWindow from '../../hooks/useSizeWindow';
 
+import { getProducts } from '../../services/api';
+
 import '../../styles/global.scss';
 
 export default function Home () {
+  const [ error, setError ] = useState<boolean>(false);
+  const { products }: any = useSelector(store => store);
+  
   const dispatch = useDispatch();
   const { isMobile } = useSizeWindow()
 
   useEffect(() => document.title = 'Ecommerce', [])
 
+  const loadProducts = async () => {
+    try {
+      const response = await getProducts();
+      dispatch(setProducts(response.data));
+    } catch (err) {
+      console.log(err)
+      setError(true);
+    } finally {
+      dispatch(setLoading(false));
+    };
+  };
+
+  useEffect(() => {
+    if(!products.catalog.length) {
+      loadProducts();
+    }
+  }, [products.catalog]);
+
 
   const handleAddProduct = (product : PropsProduct) =>  {
-    dispatch(addProductAction(product));
+    dispatch(addProductToCart(product));
   };
 
   const handleRemoveProduct = (product: PropsProduct) => {
-    dispatch(removeProductAction(product));
+    dispatch(removeProductToCart(product));
   };
+
+  if(error) {
+    return <div className='error'>Erro ao recuperar dados</div>
+  }
+
+  if(products.isLoadingProducts) {
+    return <div className='loading'>Carregando...</div>
+  }
 
   return(
     <section className="container">
       <TitlePage title="Produtos"/>
       { isMobile 
           ? <ProductsCarousel 
-              products={productList} 
+              product={products.catalog} 
               onAdd={handleAddProduct}
               onRemove={handleRemoveProduct}
             />
           : <Products 
-              products={productList} 
+              product={products.catalog} 
               onAdd={handleAddProduct}
               onRemove={handleRemoveProduct}
             />
