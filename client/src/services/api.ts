32b890487af0e4474ed3afe9d0235e23d9c16ee4
GabrielRezes <gabriel.rezes@gmail.com>
2022-04-15 17:10:14 -0300

    adding login route

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -4,7 +4,11 @@ export const api = axios.create({
   baseURL: 'http://localhost:3001' 
 }); 
 
-export const getProducts = async () => {
-    let url = '/products';
-    return api.get(url);
+export const createSession = async(email:any, password:any) => {
+  return api.post('/auth/login', {email, password});
 };
+
+export const getProducts = async () => {
+    return api.get('/products');
+};
+
