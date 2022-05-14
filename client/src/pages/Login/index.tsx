7780a0bfada7c3ebfb1bd20914d5d00af7dd1bf3
GabrielRezes <gabriel.rezes@gmail.com>
2022-05-14 00:53:03 -0300

    adding logic to login

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -7,72 +7,75 @@ import Input from '../../components/Input';
 
 import { createSession } from '../../services/api';
 
+import { Toaster, toast } from 'react-hot-toast';
 import { validateEmail, validatePassword } from '../../utils/index';
 
 export default function Login () {
 
-  const [ email, setEmail ] = useState<string>('');
-  const [ password, setPassword ] = useState<string>('');
-  const [ error, setError ] = useState<boolean>(false);
-
   const navigate = useNavigate();
 
+  const [ user, setUser ] = useState({
+    email: '',
+    password: '',
+  });
 
-  const handleLogin =  async () => {
+  const [ status, setStatus ] = useState({
+    type: '',
+    message: ''
+  });
 
-    let userEmail = email.trim().toLocaleLowerCase();
-    let userPassword = password.trim();
+  const handleChangeInputValue = (event:any) => {
+     setUser({
+       ...user,
+       [event.target.name]: event.target.value
+     });
+  };
 
-    console.log('email', email)
-    console.log('password', password)
+  const validadeData = () => {
+    if(!user.email) return setStatus({type:'error', message: 'Necessário preencher o campo email!'});
+    if(!user.password) return setStatus({type:'error', message: 'Necessário preencher o campo senha!'});
+    return true;
+  };
 
-    if(!userEmail || !userPassword) {
-      return alert ('Preencha os dados corretamente!')
-    }
-    
+  const login = async (event:any) => {
+    event.preventDefault();
+
+    if(!validadeData()) return;
+
     try {
-      const response = await createSession(userEmail, userPassword);
-      console.log(response.data)
+      const { data } = await createSession(user);
+
+      if(data) {
+        localStorage.setItem('user', JSON.stringify(data.user));
+        localStorage.setItem('token', JSON.stringify(data.token));
+      }
+
+      setUser({email:'',password:''});
+      setStatus({type:'success', message:'Login com Sucesso!'});
+
+      toast.success('Login com Sucesso!');
+
+      setTimeout(() => {navigate('/')}, 2000);
+      
     } catch (err) {
-      console.log('error')
-      setError(true);
-    } finally {
-      return console.log('deu certo')
-    }
+      setStatus({type:'error', message: 'Ocorreu um erro ao realizar o Login!'});
 
-    // let userEmail = validateEmail(email);
-    // let userPassword = validatePassword(password);
-
-    // if(!userEmail || !userPassword) {
-    //   return alert('Informe um email válido');
-    // }
-
-    // if(!userPassword) {
-    //   console.log('deu erro')
-    //   return alert (`
-    //     A senha deve conter:
-    //     - Mínimo de oito caracteres
-    //     - Mínimo de um número
-    //     - Mínimo de um caractere especial
-    //     - Mínimo de uma letra maiúscula
-    //   `)
-    // }
-
-
-
-  }
+      toast.error('Ocorreu um erro ao realizar o Login!');
+    };
+  };
 
   return (
     <section className='container'>
+      <div><Toaster position="top-center" reverseOrder={false}/></div>
+
       <TitlePage title='Login'/>
 
       <div className='container-input'>
-        <Input labelText='Email' inputName='email' inputType='email' value={email} setValue={setEmail}/>
-        <Input labelText='Senha' inputName='password' inputType='password' value={password} setValue={setPassword}/>
+        <Input labelText='Email' inputName='email' inputType='email' value={user.email} setValue={handleChangeInputValue}/>
+        <Input labelText='Senha' inputName='password' inputType='password' value={user.password} setValue={handleChangeInputValue}/>
       </div>  
-
-        <Button text='Entrar' variant='primary' onClick={handleLogin}/>
-        <Button text='Cadastrar' variant='primary' onClick={() => navigate('./register')}/>
+          <span onClick={() => navigate('/createAccount')}>Clique aqui para criar uma conta</span>
+        <Button text='Entrar' variant='primary' onClick={login}/>
     </section>
   );
 };
