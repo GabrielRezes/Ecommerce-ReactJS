fd6465f9ed56dd746996910cbc3407b7eaf107bb
GabrielRezes <gabriel.rezes@gmail.com>
2022-05-14 00:53:54 -0300

    adding path to create account

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -4,6 +4,7 @@ import Home from '../pages/Home';
 import Cart from '../pages/Cart';
 import About from '../pages/About';
 import Login from '../pages/Login';
+import CreateAccount from '../pages/CreateAccount'
 
 export default function AppRoutes () {
   return (
@@ -12,6 +13,7 @@ export default function AppRoutes () {
       <Route path='/about' element={<About/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/cart' element={<Cart/>}/>
+      <Route path='/createAccount' element={<CreateAccount/>}/>
     </Routes>
   );
 };
