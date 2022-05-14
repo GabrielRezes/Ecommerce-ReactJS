1bf06b1fe7772bdd8cef4a338e617fb19171098f
GabrielRezes <gabriel.rezes@gmail.com>
2022-05-14 00:53:37 -0300

    adding api route to create account

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -4,11 +4,16 @@ export const api = axios.create({
   baseURL: 'http://localhost:3001' 
 }); 
 
-export const createSession = async(email:any, password:any) => {
+export const createUser = async ({name, email, password}:any) => {
+  return api.post('/auth/register', {name, email, password} );
+};
+
+export const createSession = async({email, password}:any) => {
   return api.post('/auth/login', {email, password});
 };
 
 export const getProducts = async () => {
     return api.get('/products');
 };
+
 
