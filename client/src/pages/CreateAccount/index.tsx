3db5544a0d9dff1907143b8c4d9d185c3f293e75
GabrielRezes <gabriel.rezes@gmail.com>
2022-05-14 22:55:36 -0300

    adding toast error

--- client/src/pages/CreateAccount/index.tsx
+++ client/src/pages/CreateAccount/index.tsx
@@ -4,9 +4,10 @@ import { useNavigate } from 'react-router-dom'
 import TitlePage from '../../components/TitlePage';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
-import Status from '../../components/Status'; 
 
 import { createUser } from '../../services/api'; 
+
+import { Toaster, toast } from 'react-hot-toast';
 
 export default function CreateAccount () {
 
@@ -19,34 +20,29 @@ export default function CreateAccount () {
     confirmPassword: ''
   });
 
-  const [ status, setStatus ] = useState({
-    type: '',
-    message: ''
-  });
-
   const validateData = () => {
     if(!user.name) {
-      setStatus({type:'error', message: 'Necessário preencher o campo nome!'});
+      toast.error('Necessário preencher o campo nome!')
       return false 
     } 
     if(!user.email) {
-      setStatus({type:'error', message: 'Necessário preencher o campo email!'});
+      toast.error('Necessário preencher o campo email!')
       return false
     } 
     if(!user.password) {
-      setStatus({type:'error', message: 'Necessário preencher o campo senha!'});
+      toast.error('Necessário preencher o campo senha!')
       return false
     }  
     if(!user.confirmPassword) { 
-      setStatus({type:'error', message: 'Necessário preencher o campo senha novamente!'});
+      toast.error('Necessário preencher o campo senha novamente!')
       return false
     }  
     if(user.password.length < 6) {
-      setStatus({type:'error', message: 'A senha precisa ter no mínimo 6 caracteres!'});
+      toast.error('A senha precisa ter no mínimo 6 caracteres!')
       return false
     }  
     if(user.password !== user.confirmPassword) {
-      setStatus({type:'error', message: 'As senhas não conferem'})
+      toast.error('As senhas não conferem!')
       return false
     }  
 
@@ -62,33 +58,29 @@ export default function CreateAccount () {
 
   const createAccount = async (event:any) => {
     event.preventDefault();
-    
     const isValid = validateData();
-    isValid ? postUser() : '';
-  }
+    isValid && postUser();
+  };
   
   const postUser =  async () => {
     try{
       const response = await createUser(user);
-      console.log(response)
-      setUser({name:'',email:'',password:'',confirmPassword:''})
-      setStatus({type:'success', message:'Usuário criado com Sucesso! Você será redirecionado para a página de Login'})
-      
-      setTimeout(() => navigate('/login'),4000)
+      console.log(response);
+      toast.success('Usuário criado com Sucesso! Você será redirecionado para a página de Login');
+      setTimeout(() => navigate('/login'),4000);
 
     } catch (err) {
-      console.log(err)
-      setStatus({type:'error', message: 'Ocorreu um erro ao criar a conta, tente mais tarde!'});
-
-    } finally {
-      return console.log(status)
-    }
-  }  
+      console.log(err);
+      toast.success('Ocorreu um erro ao criar a conta, tente mais tarde!');
+      setUser({name: '', email: '', password: '', confirmPassword: ''});
+    };
+  };
 
   return (
     <section className='container'>
-      <TitlePage title='Crie sua Conta'/>
+      <div><Toaster position="top-center" reverseOrder={false}/></div>
 
+      <TitlePage title='Crie sua Conta'/>
 
       <div className='container-input'>
         <Input labelText='Nome' inputName='name' inputType='text' value={user.name} setValue={handleChangeInputValue}/>
@@ -96,8 +88,6 @@ export default function CreateAccount () {
         <Input labelText='Senha' inputName='password' inputType='password' value={user.password} setValue={handleChangeInputValue}/>
         <Input labelText='Confirme a senha' inputName='confirmPassword' inputType='password' value={user.confirmPassword} setValue={handleChangeInputValue}/>
       </div>
-      {status.type === 'error' && <Status text={status.message} type={status.type}/>}
-      {status.type === 'success' && <Status text={status.message} type={status.type}/>}
         
       <Button text='Entrar' variant='primary' onClick={createAccount}/>
     </section>
