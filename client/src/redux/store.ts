e6e3b6a5d0a292f1ee3e05a149a1b18cb84a7017
GabrielRezes <gabriel.rezes@gmail.com>
2022-05-14 22:56:12 -0300

    adding new reducer

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,11 +1,15 @@
 import { createStore, combineReducers } from 'redux';
+
 import cartReducer from './reducers/cartReducer';
 import productReducer from './reducers/productReducer';
+import userReducer from './reducers/userReducer';
+
 import { PropsProduct } from '../types';
 
 const reducers = combineReducers({
   cart: cartReducer,
   products: productReducer,
+  userInfo: userReducer
 });
 
 const store = createStore(reducers);
