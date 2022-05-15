d94e87a63dcb720679875725e7c76d0bb22c9dc4
GabrielRezes <gabriel.rezes@gmail.com>
2022-05-14 22:56:27 -0300

    adding new routes

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -1,12 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
 
 import Home from '../pages/Home';
 import Cart from '../pages/Cart';
 import About from '../pages/About';
 import Login from '../pages/Login';
+import AdmUser from '../pages/AdmUser';
+import NotFoundPage from '../pages/NotFoundPage';
 import CreateAccount from '../pages/CreateAccount'
 
 export default function AppRoutes () {
+
+  const { userInfo }:any = useSelector(store => store);
+
+  const PrivateRoute = ({children}:any) => {
+    if(!userInfo.id) {
+      return <Navigate to='/'/>
+    } 
+    return children;
+  }
+
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
@@ -14,6 +28,8 @@ export default function AppRoutes () {
       <Route path='/login' element={<Login/>}/>
       <Route path='/cart' element={<Cart/>}/>
       <Route path='/createAccount' element={<CreateAccount/>}/>
+      <Route path='/admUser' element={<PrivateRoute><AdmUser/></PrivateRoute>}/>
+      <Route path="*" element={<NotFoundPage />}/>
     </Routes>
   );
 };
